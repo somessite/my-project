e19cde3e55196b385fc923eb0beccffd5f88bc6b
somessite <57861444+somessite@users.noreply.github.com>
2020-04-28 18:09:29 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ const Home = () => (
 
     <main>
       <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js! Hello World!!!</a>
+        Deploy to Heroku Next js
       </h1>
 
       <p className="description">
