f30f5c4a179d040e90119cb8324876334f1e51cd
somessite <57861444+somessite@users.noreply.github.com>
2020-04-28 18:13:30 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,33 +17,7 @@ const Home = () => (
       </p>
 
       <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://zeit.co/new?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
+         <img src="https://sun2.48276.userapi.com/UdfUI6aLEM9Ts0391M00XK_r9q9mkxIYLMjCpw/jX5qmX_LOac.jpg" alt="it"/>
       </div>
     </main>
 
